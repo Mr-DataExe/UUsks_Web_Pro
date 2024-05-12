13c4bb21377690301ff8567b1f3d9c1f6fe30674
Mr-DataExe <mohammedad.work@gmail.com>
2024-05-13 00:54:39 +0300

    Added Login And Regestration With rederect

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const dashboardContent = document.getElementById('tab-cont');
 const element1 = document.querySelector('.cs');
 const element2 = document.querySelector('.log-in');
 const passwordInput = document.getElementById('password');
+const newUser = document.getElementById('newUserName');
+const newEmail = document.getElementById('newEmail');
+const newPassword = document.getElementById('newPassword');
+const newRePassword = document.getElementById('newRePassword');
+const newDate = document.getElementById('newDate');
+const redTextUser = document.getElementById('username-error');
+const redTextEmail = document.getElementById('email-error');
+const redTextPassword = document.getElementById('password-error');
+const redTextRePassword = document.getElementById('re-password-error');
+const redTextDate = document.getElementById('birth-error');
 const redText = document.querySelector('.log-in .spef .cont .bottom-elements .wrong')
 
 sideLinks.forEach(item => {
@@ -57,6 +67,7 @@ function loadClubsContent() {
             console.error('There was a problem fetching the dashboard content:', error);
         });
 }
+
 const getUsers = async () => {
     try {
     const response = await fetch('https://clubs-system.onrender.com/api/user/');
@@ -78,22 +89,39 @@ const getUsers = async () => {
 // Function to handle user signup
 const signUpUser = async (userData) => {
     try {
-      const response = await fetch('https://clubs-system.onrender.com/api/user/signup', {
+        const response = await fetch('https://clubs-system.onrender.com/api/user/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(userData),
-      });
-      const data = await response.json();
-      console.log(data);
+        });
+        if (response.ok) {
+            goTo();
+            const data = await response.json();
+            console.log(data);
+        }else{
+                redText.style.display = 'flex';
+        }
     } catch (error) {
       console.error('Error:', error);
     }
 };
-  
-  // Function to handle user login
-  const loginUser = async (loginData) => {
+
+function onLoadCheckLog() {
+    let localData = localStorage.getItem("myData");
+    let sessionData = sessionStorage.getItem("myData");
+    if(localData !== null) {
+        console.log(localData)
+    }else if(sessionData !== null) {
+        console.log(sessionData)
+    }else if (localData == null || sessionData == null) {
+        window.location.href = 'Login.html';
+    }
+}
+
+// Function to handle user login
+const loginUser = async (loginData) => {
     try {
         const response = await fetch('https://clubs-system.onrender.com/api/user/login', {
             method: 'POST',
@@ -103,11 +131,18 @@ const signUpUser = async (userData) => {
         body: JSON.stringify(loginData),
         });
         if (response.ok) {
-            goTo();
             const data = await response.json();
-            console.log(data);
+
+            if (document.getElementById("myCheckbox").checked) {
+                localStorage.setItem("myData", data.user._id);
+                console.log("Data saved to localStorage");
+            } else {
+                sessionStorage.setItem("myData", data.user._id);
+                console.log("Data saved to sessionStorage");
+            }
+            goTo();
         }else{
-            redText.style.display = 'contents';
+            redText.style.display = 'flex';
         }
     }catch (error) {
         console.error('Error:', error);
@@ -115,14 +150,76 @@ const signUpUser = async (userData) => {
 };
 
 // Example usage
-const signUpData = {
-    user_img: 'images/yapay-zeka-kulubu 1.png',
-    user_name: 'Admin',
-    user_email: 'admin@example.com',
-    user_password: 'password123',
-    user_birth: '1995',
-};
-function check(){
+function regester(){
+    const userNewImg = ('image/profile-default.svg');
+    const userNewName = document.getElementById('newUserName').value;
+    const userNewEmail = document.getElementById('newEmail').value;
+    const userNewPassword = document.getElementById('newPassword').value;
+    const userNewRePassword = document.getElementById('newRePassword').value;
+    const userNewDate = document.getElementById('newDate').value;
+
+    let userCheck = true;
+    let emailCheck = true;
+    let passwordCheck = true;
+    let rePasswordCheck = true;
+    let dateCheck = true;
+
+    if (userNewName.length < 3) {
+        newUser.style.borderColor = '#ff000090';
+        redTextUser.style.display = 'flex'
+        userCheck = false;
+    }else{
+        newUser.style.borderColor = '#25252c';
+        userCheck = true;
+    }
+    if (!userNewEmail.includes('@st.uskudar.edu.tr' || '@uskudar.edu.tr') || userNewEmail.length < 20) {
+        newEmail.style.borderColor = '#ff000090';
+        redTextEmail.style.display = 'flex'
+        emailCheck = false;
+    }else{
+        newEmail.style.borderColor = '#25252c';
+        emailCheck = true;
+    }
+    if (/^[a-zA-Z]+$/.test(userNewPassword) || /^[0-9]+$/.test(userNewPassword) || userNewPassword.length < 8) {
+        newPassword.style.borderColor = '#ff000090';
+        redTextPassword.style.display = 'flex'
+        passwordCheck = false;
+    }else{
+        newPassword.style.borderColor = '#25252c';
+        passwordCheck = true;
+    }
+    if (userNewRePassword != userNewPassword) {
+        newRePassword.style.borderColor = '#ff000090';
+        redTextRePassword.style.display = 'flex'
+        rePasswordCheck = false;
+    }else{
+        newRePassword.style.borderColor = '#25252c';
+        rePasswordCheck = true;
+    }
+    if (userNewDate.length < 10) {
+        newDate.style.borderColor = '#ff000090';
+        redTextDate.style.display = 'flex'
+        dateCheck = false;
+    }else{
+        newDate.style.borderColor = '#25252c';
+        dateCheck = true;
+    }
+    if (userCheck == false || emailCheck == false || passwordCheck == false || rePasswordCheck == false || dateCheck == false) {
+        return;
+    }
+    return;
+    const signUpData = {
+        user_img: userNewImg,
+        user_name: userNewName,
+        user_email: userNewEmail,
+        user_password: userNewPassword,
+        user_birth: userNewDate,
+    };
+    console.log(signUpData);
+    signUpUser(signUpData)
+}
+
+function login(){
     const userEmail = document.getElementById('email').value;
     const userPassword = document.getElementById('password').value;
     const loginData = {
@@ -144,7 +241,13 @@ function back() {
 }
 
 function logOut(){
+    clearData();
     window.location.href = 'Login.html';
+}
+
+function clearData() {
+    localStorage.removeItem("myData");
+    sessionStorage.removeItem("myData");
 }
 
 function goTo() {
